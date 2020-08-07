cde412764dfceeecb8d78abbc9141b522ea6b56f
qkrwlsaud <qkrwlsaud0118@gmail.com>
2020-08-07 18:21:00 +0900

    Member Controller

--- Spring/APP-Member-Rest/src/main/java/com/aia/mm/controller/MemberRestController.java
+++ Spring/APP-Member-Rest/src/main/java/com/aia/mm/controller/MemberRestController.java
@@ -2,16 +2,25 @@ package com.aia.mm.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aia.mm.model.Member;
+import com.aia.mm.model.MemberEditRequest;
 import com.aia.mm.model.MemberRegRequest;
+import com.aia.mm.service.MemberDeleteServcie;
+import com.aia.mm.service.MemberEditService;
 import com.aia.mm.service.MemberListService;
+import com.aia.mm.service.MemberRegService;
+import com.aia.mm.service.MemberViewService;
 
 @RestController
 @RequestMapping("/members")
@@ -19,6 +28,18 @@ public class MemberRestController {
 
 	@Autowired
 	private MemberListService listService;
+	
+	@Autowired 
+	private MemberRegService regService;
+	
+	@Autowired
+	private MemberViewService viewServie;
+	
+	@Autowired
+	private MemberEditService editService;
+	
+	@Autowired
+	private MemberDeleteServcie delteService;
 	
 	//회원의 리스트 : Json 으로 응답 
 	@GetMapping 		//Get 	|	/members
@@ -28,25 +49,37 @@ public class MemberRestController {
 	
 	// 회원 등록
 	@PostMapping 		//Post 	|	/members
-	public int reg(MemberRegRequest regRequest) {
-		return 0;
+	public int reg(MemberRegRequest regRequest, HttpServletRequest request) {
+		
+		return regService.regMember(regRequest, request);
 	}
 	
 	// 한 명의 회원 정보 보기
 	@GetMapping("/{idx}")
-//	public Member getMember() {
-//		return null;
-//	}
+	public Member getMember(
+			@PathVariable("idx") int idx
+			) {
+		
+		return viewServie.getMember(idx);
+	}
 	
 	// 한 명의 회원 정보 수정
-	@PutMapping("/{idx}") 	//Put | /members/{idx}
-	public int edit() {
-		return 0;
+	//@PutMapping("/{idx}") 	//Put | /members/{idx}
+	@PostMapping("/{idx}")
+	public int edit(
+			@PathVariable("idx") int idx,
+			MemberEditRequest editRequest,
+			HttpServletRequest request
+			) {
+		editRequest.setIdx(idx);
+		
+		return editService.editMember(editRequest, request);
 	}
 	
 	// 한 명의 회원 정보 삭제 
-	public int delete() {
-		return 0;
+	@DeleteMapping("/{idx}")
+	public int delete(@PathVariable("idx") int idx) {
+		return delteService.deleteMember(idx);
 	}
 	
 }
